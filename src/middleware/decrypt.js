5ea40d6e9ead041e6ee6214f215f21520972d659
Suyog Amin <suyogamin@Suyogs-MacBook-Air.local>
2025-11-20 16:47:30 +0530

    aes issue fix

--- src/middleware/decrypt.js
+++ src/middleware/decrypt.js
@@ -2,40 +2,42 @@ const CryptoJS = require("crypto-js");
 const { getAESKeyForUser } = require("./aes");
 
 function decryptPayload(req, res, next) {
-  try {
-    const email = req.user.email; // Use email as userId from JWT token
-    const aesKey = getAESKeyForUser(email); // base64 key
-    console.log('AES Key:', aesKey);
-    const key = CryptoJS.enc.Base64.parse(aesKey);
+    try {
+        const email = req.user.email; // Use email as userId from JWT token
+        const aesKey = getAESKeyForUser(email); // base64 key
+        const key = CryptoJS.enc.Base64.parse(aesKey);
 
-    
-    const { cipher, iv } = req.body.payload;
+        if (!aesKey) {
+            return res.status(440).json({ success: false, msg: "AES_KEY_EXPIRED. Please reload or logout" });
+        }
 
-    if (!cipher || !iv) {
-      return res.status(400).json({ 
-        success: false, 
-        msg: "Missing encrypted payload (cipher or iv)" 
-      });
+        const { cipher, iv } = req.body.payload;
+
+        if (!cipher || !iv) {
+            return res.status(400).json({
+                success: false,
+                msg: "Missing encrypted payload (cipher or iv)"
+            });
+        }
+
+        const decrypted = CryptoJS.AES.decrypt(cipher, key, {
+            iv: CryptoJS.enc.Base64.parse(iv),
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7,
+        });
+
+        const json = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
+
+        req.body = json;
+
+        next();
+    } catch (err) {
+        console.error("AES decrypt error:", err);
+        return res.status(400).json({
+            success: false,
+            msg: "Invalid encrypted payload.. Please reload the screen and try again."
+        });
     }
-
-    const decrypted = CryptoJS.AES.decrypt(cipher, key, {
-      iv: CryptoJS.enc.Base64.parse(iv),
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-    });
-
-    const json = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
-
-    req.body = json;
-
-    next();
-  } catch (err) {
-    console.error("AES decrypt error:", err);
-    return res.status(400).json({ 
-      success: false, 
-      msg: "Invalid encrypted payload" 
-    });
-  }
 }
 
 module.exports = { decryptPayload };
